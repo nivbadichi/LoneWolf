f1e8100ba7dbbc2d550b05f39793011f19ec2e96
Niv-code <nivhor0@gmail.com>
2026-06-19 14:00:33 +0300

    Module Events
    finished Module events

--- code/server/server.js
+++ code/server/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use('/api/events', eventRoutes);
 app.get("/", (req, res) => {
   res.send("LoneWolf API is running");
 });
