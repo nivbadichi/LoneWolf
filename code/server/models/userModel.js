6ccc73f8e5bbb0363683fe19278357f84a2b123f
Niv Badichi <nivbad88@gmail.com>
2026-06-19 13:48:15 +0300

    wrote entire user model

---
+++ code/server/models/userModel.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    interests: {
+      type: [String],
+      default: [],
+    },
+    isBanned: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
